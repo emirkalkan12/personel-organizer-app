e810a0ac0a48eaa01458d74e3a89a6509284d9ed
Emirhan Kalkan <147448935+emirkalkan12@users.noreply.github.com>
2025-05-12 12:53:17 +0300

    Add password recovery form and alarm system components

---
+++ ForgetPassword.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace personel_organizer_ilk
+{
+    partial class ForgetPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ForgetPassword));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblMesaj = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxEmail = new System.Windows.Forms.TextBox();
+            this.txtBoxUsename = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lblStatus);
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.lblMesaj);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtBoxEmail);
+            this.panel1.Controls.Add(this.txtBoxUsename);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(94, 126);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(416, 401);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(175, 375);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(44, 16);
+            this.lblStatus.TabIndex = 14;
+            this.lblStatus.Text = "label5";
+            this.lblStatus.Visible = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(178, 327);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 13;
+            this.progressBar1.Visible = false;
+            // 
+            // lblMesaj
+            // 
+            this.lblMesaj.AutoSize = true;
+            this.lblMesaj.Location = new System.Drawing.Point(163, 418);
+            this.lblMesaj.Name = "lblMesaj";
+            this.lblMesaj.Size = new System.Drawing.Size(0, 16);
+            this.lblMesaj.TabIndex = 12;
+            this.lblMesaj.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SteelBlue;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(142, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 30);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "SEND";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(5, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 27);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(5, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 27);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "E-mail";
+            // 
+            // txtBoxEmail
+            // 
+            this.txtBoxEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtBoxEmail.Location = new System.Drawing.Point(142, 191);
+            this.txtBoxEmail.Multiline = true;
+            this.txtBoxEmail.Name = "txtBoxEmail";
+            this.txtBoxEmail.Size = new System.Drawing.Size(203, 27);
+            this.txtBoxEmail.TabIndex = 4;
+            // 
+            // txtBoxUsename
+            // 
+            this.txtBoxUsename.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtBoxUsename.Location = new System.Drawing.Point(142, 248);
+            this.txtBoxUsename.Multiline = true;
+            this.txtBoxUsename.Name = "txtBoxUsename";
+            this.txtBoxUsename.Size = new System.Drawing.Size(203, 27);
+            this.txtBoxUsename.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.SteelBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(416, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Forget Password";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(410, 95);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(69, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 25);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "HOME";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox2.BackgroundImage = global::personel_organizer_ilk.Properties.Resources.backk;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(10, 11);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(53, 44);
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // ForgetPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 643);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "ForgetPassword";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ForgetPassword";
+            this.Load += new System.EventHandler(this.ForgetPassword_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblMesaj;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxUsename;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ ForgetPassword.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace personel_organizer_ilk
+{
+    public partial class ForgetPassword : Form
+    {
+        private string sentCode = "";
+        private string targetUsername = "";
+
+        public ForgetPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void ForgetPassword_Load(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Properties.Resources.loginpage;
+        }
+
+        private void SendPasswordResetEmail(string userEmail, string resetCode)
+        {
+            try
+            {
+                // 
+                lblStatus.Text = "Sending email...";
+                lblStatus.Visible = true;
+                progressBar1.Style = ProgressBarStyle.Marquee;
+                progressBar1.Visible = true;
+                progressBar1.MarqueeAnimationSpeed = 30;
+
+                Application.DoEvents(); 
+                MailMessage mail = new MailMessage();
+                SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("abcde2@gmail.com");
+                mail.To.Add(userEmail);
+                mail.Subject = "Password Reset Request";
+                mail.Body = $"Merhaba,\n\nŞifre sıfırlama kodunuz: {resetCode}\n\nBu işlemi siz başlatmadıysanız lütfen bu e-postayı dikkate almayınız.";
+
+                smtpServer.Port = 587;
+                smtpServer.Credentials = new NetworkCredential("abcde@gmail.com", "abcde fgh klmn");
+                smtpServer.EnableSsl = true;
+
+                smtpServer.Send(mail);
+
+                // 
+                lblStatus.Text = "Email sent successfully.";
+                progressBar1.Visible = false;
+
+                MessageBox.Show("Password reset code has been sent to your email address.", "SUCCESSFUL", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                
+                lblStatus.Text = "Failed to send email.";
+                progressBar1.Visible = false;
+
+                MessageBox.Show("Email could not be sent:" + ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputUsername = txtBoxUsename.Text.Trim();
+            string inputEmail = txtBoxEmail.Text.Trim();
+            string path = Login.path;
+
+            List<User> userList = new List<User>();
+            Util.LoadCsv(userList, path);
+
+            User foundUser = userList.FirstOrDefault(u =>
+                u.Username.Equals(inputUsername, StringComparison.OrdinalIgnoreCase) &&
+                u.Email.Equals(inputEmail, StringComparison.OrdinalIgnoreCase));
+
+            if (foundUser != null)
+            {
+                string resetCode = Guid.NewGuid().ToString().Substring(0, 6);
+                sentCode = resetCode;
+                targetUsername = foundUser.Username;
+
+                SendPasswordResetEmail(foundUser.Email, resetCode);
+
+                this.Hide();
+                ResetPasswordForm resetForm = new ResetPasswordForm(targetUsername, sentCode);
+                resetForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Username or email is incorrect!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login yeniForm = new Login();
+            yeniForm.Show();
+        }
+    }
+}
